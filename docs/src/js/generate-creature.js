4f3c7c07b5688fe86f85711bba3a76f852007353
James <jblac150@uncc.edu>
2020-02-20 01:06:52 -0500

    Working on Abilities generation, Add randomize button
    The abilities will be generated depending on the type of creature, as some type seem to favor certain stats over others.

--- docs/src/js/generate-creature.js
+++ docs/src/js/generate-creature.js
@@ -6,12 +6,18 @@ function calcAll(){
     calcAvgHP(CR);
     calcSizeTypeAlign(CR);
     calcProfBonus(CR);
+    calcAvgAtkBonus(CR);
     calcAbilities(CR);
 }
 
 function calcAC(CR){
     AC = Math.floor(13 + (1.0 / 3.0) * CR + (Math.random() * 6.0) - 3.0);
     document.getElementById("AC").innerHTML = AC;
+}
+
+// Average damage the monster should be dealing each turn, used by other functions
+function calcAvgAtkBonus(CR){
+    avgAtkBonus = Math.floor(4.0 + ((1.0 / 2.0) * CR + ((Math.random() * 2.0) - 1.0)));
 }
 
 // Average damage the monster should be dealing each turn, used by other functions
@@ -26,9 +32,9 @@ function calcAvgHP(CR){
 }
 
 function calcSizeTypeAlign(CR){
-    if(CR < 2) {    
+    if(CR < 2){    
         // monster below CR 2 have a size range between tiny and large
-        switch (Math.floor(Math.random() * 4.0)) {
+        switch(Math.floor(Math.random() * 4.0)){
             case 0:
                 size = "Tiny";
                 break;
@@ -43,8 +49,8 @@ function calcSizeTypeAlign(CR){
                 break;
         }
     //anything between CR 2 and 10 should have a size of medium to huge
-    } else if (CR >= 2 && CR < 11) {
-        switch (Math.floor(Math.random() * 3.0)) {
+    } else if(CR >= 2 && CR < 11){
+        switch (Math.floor(Math.random() * 3.0)){
             case 0:
                 size = "Medium";
                 break;
@@ -56,8 +62,8 @@ function calcSizeTypeAlign(CR){
                 break;
         }
     //anything between CR 11 and 21 should be between size medium and gargantuan
-    } else if (CR >= 11 && CR < 22) {    
-        switch (Math.floor(Math.random() * 3.0)) {
+    } else if(CR >= 11 && CR < 22){    
+        switch (Math.floor(Math.random() * 3.0)){
             case 0:
                 size = "Medium";
                 break;
@@ -72,12 +78,12 @@ function calcSizeTypeAlign(CR){
                 break;
         }
     //anything more than CR 21 should be gargantuan in size
-    } else {
+    } else{
         size = "Gargantuan";
     }
     
     //sets the type at random
-    switch (Math.floor(Math.random() * 14.0)) {
+    switch(Math.floor(Math.random() * 14.0)){
         case 0:
             type = "aberration";
             break;
@@ -121,7 +127,7 @@ function calcSizeTypeAlign(CR){
             type = "undead";
             break;
     }
-    
+
     // Alignment will have to be researched, it is likely tied to type
     var align = "unaligned";
     var sizeTypeAlign = size + " " + type + ", " + align;
@@ -139,13 +145,152 @@ function calcProfBonus(CR){
 }
 
 function calcAbilities(CR){
-    STR = 20;
-    // Randomly figure out if the monster will primarily use DEX or STR
-    usesFinesse = Math.random() >= 0.5;
-    /*if(usesFinesse){
+    // Initialize the ability scores
+    var abilities = [10, 10, 10, 10, 10, 10];
 
-    } else{
+    // Randomly figure out what the primary damage stat is
+    switch(Math.floor(Math.random() * 2.0)){
+        case 0:
+            // Set DEX as the primary damage stat
+            abilities[1] = ((avgAtkBonus - profBonus) * 2) + 10;
+            // Set STR
+            abilities[0] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+            // Set INT
+            abilities[3] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+            // Set WIS
+            abilities[4] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+            break;
+        case 1:
+            // Set STR as the primary damage stat
+            abilities[0] = ((avgAtkBonus - profBonus) * 2) + 10;
+            // Set DEX
+            abilities[1] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+            // Set INT
+            abilities[3] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+            // Set WIS
+            abilities[4] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+            break;
+        case 2:
+            // Set INT as the primary damage stat
+            abilities[3] = ((avgAtkBonus - profBonus) * 2) + 10;
+            // Set DEX
+            abilities[1] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+            // Set STR
+            abilities[0] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+            // Set WIS
+            abilities[4] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+            break;
+        case 3:
+            // Set WIS as the primary damage stat
+            abilities[4] = ((avgAtkBonus - profBonus) * 2) + 10;
+            // Set DEX
+            abilities[1] = abilities[4] - Math.floor(Math.random() * (abilities[4] / 2));
+            // Set INT
+            abilities[3] = abilities[4] - Math.floor(Math.random() * (abilities[4] / 2));
+            // Set STR
+            abilities[0] = abilities[4] - Math.floor(Math.random() * (abilities[4] / 2));
+            break;
+    }
 
-    }*/
-    document.getElementsByTagName("abilities-block")[0].setAttribute("data-str", STR);
+    // TODO: Set CON
+
+    // Set CHA
+    abilities[5] = Math.floor(abilities[5] + (1.0 / 3.0) * (CR));
+
+    updateAbilities(abilities);
+}
+
+// Some types do not use certain stats to fight
+function calcPrimaryStat(CR){
+    //sets the type at random
+    switch(type){
+        // Aberrations seem to use STR, DEX, and INT as primary stats
+        case "aberration":
+            switch(Math.floor(Math.random() * 3.0)){
+                case 0:
+                    // Set DEX as the primary damage stat
+                    abilities[1] = ((avgAtkBonus - profBonus) * 2) + 10;
+                    // Set STR
+                    abilities[0] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+                    // Set INT
+                    abilities[3] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+                    // Set WIS
+                    abilities[4] = abilities[1] - Math.floor(Math.random() * (abilities[1] / 2));
+                    break;
+                case 1:
+                    // Set STR as the primary damage stat
+                    abilities[0] = ((avgAtkBonus - profBonus) * 2) + 10;
+                    // Set DEX
+                    abilities[1] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+                    // Set INT
+                    abilities[3] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+                    // Set WIS
+                    abilities[4] = abilities[0] - Math.floor(Math.random() * (abilities[0] / 2));
+                    break;
+                case 2:
+                    // Set INT as the primary damage stat
+                    abilities[3] = ((avgAtkBonus - profBonus) * 2) + 10;
+                    // Set DEX
+                    abilities[1] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+                    // Set STR
+                    abilities[0] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+                    // Set WIS
+                    abilities[4] = abilities[3] - Math.floor(Math.random() * (abilities[3] / 2));
+                    break;
+            }
+            break;
+        case "beast":
+            break;
+        case "celestial":
+            break;
+        case "construct":
+            break;
+        case "dragon":
+            break;
+        case "elemental":
+            break;
+        case "fey":
+            break;
+        case "fiend":
+            break;
+        case "giant":
+            break;
+        case "humanoid":
+            break;
+        case "monstrosity":
+            break;
+        case "ooze":
+            break;
+        case "plant":
+            break;
+        case "undead":
+            break;
+    }
+}
+
+function updateAbilities(abilities){
+    var abilityNames = ["str", "dex", "con", "int", "wis", "cha"];
+
+    // Update the abilities block
+    for (var i = 0; i < abilities.length; i++){
+        document.getElementsByTagName("abilities-block")[0].shadowRoot.getElementById(abilityNames[i]).innerText =
+            abilities[i] + " (" + formattedModifier(abilityModifier(abilities[i])) + ")";
+    }
+}
+
+// From abilities-block.js. Having import issues so I just moved the functions I need here
+function abilityModifier(abilityScore){
+    let score = parseInt(abilityScore, 10);
+    return Math.floor((score - 10) / 2);
+}
+
+// Also from abilities-block.js
+function formattedModifier(abilityModifier){
+    if (abilityModifier >= 0){
+      return '+' + abilityModifier;
+    }
+
+    // This is an en dash, NOT a "normal" dash. The minus sign needs to be more
+    // visible.
+    return '–' + Math.abs(abilityModifier);
 }
